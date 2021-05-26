194292d2b40ddd645a0edc4b41421c62bee1ae9d
Devon Osei <oseidevon@gmail.com>
2021-05-26 17:20:46 -0400

    componentss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Signup from './components/SignUp'
+import NavBar from './components/NavBar';
+import Recipe from './components/Recipe'
 import './App.css';
+import NewRecipe from './components/NewRecipe';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Landing />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+          <Route path='/recipe/:id'>
+            <Recipe />
+          </Route>
+          <Route path='/newRecipe'>
+          <NewRecipe />
+          </Route>
+          {/* testing */}
+          <Route path='/nav'>
+            <NavBar />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
